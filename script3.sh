0b47e2e298a7d16161d29a872b6000fa96ddf46f
Eliot008 <47475642+Eliot008@users.noreply.github.com>
2022-05-10 11:20:06 +0300

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -3,21 +3,21 @@
 user=$(whoami) # Змінні user за допомогою команди whoami присвоюємо значення(щоб зрозуміти хтоми є)
 
 if [ "$user" == "root" ]; then # Дивимося чи змінна user це root
-        # Якщо змінна user це root, то записуємо нижче наведене в script3.txt
-        uptime > /$user/scripts/script3.txt
-        who -a >> /$user/scripts/script3.txt
-        lastlog >> /$user/scripts/script3.txt
-        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head >> /$user/scripts/script3.txt
-        sudo netstat -tulpn | grep LISTEN >> /$user/scripts/script3.txt
-        free -t >> /$user/scripts/script3.txt
-        df >> /$user/scripts/script3.txt
+        # Якщо змінна user це root, то записуємо нижче наведене в script3.log
+        uptime > /$user/scripts/script3.log
+        who -a >> /$user/scripts/script3.log
+        lastlog >> /$user/scripts/script3.log
+        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head >> /$user/scripts/script3.log
+        sudo netstat -tulpn | grep LISTEN >> /$user/scripts/script3.log
+        free -t >> /$user/scripts/script3.log
+        df >> /$user/scripts/script3.log
 else
         # Якщо змінна user це не root, то записуємо нижче наведене в script2.txt, але зверніть увагу на шляхи
-        uptime > /home/$user/scripts/script3.txt
-        who -a >> /home/$user/scripts/script3.txt
-        lastlog >> /home/$user/scripts/script3.txt
-        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head >> /home/$user/scripts/script3.txt
-        sudo netstat -tulpn | grep LISTEN >> /home/$user/scripts/script3.txt
-        free -t >> /home/$user/scripts/script3.txt
-        df >> /home/$user/scripts/script3.txt
+        uptime > /home/$user/scripts/script3.log
+        who -a >> /home/$user/scripts/script3.log
+        lastlog >> /home/$user/scripts/script3.log
+        ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head >> /home/$user/scripts/script3.log
+        sudo netstat -tulpn | grep LISTEN >> /home/$user/scripts/script3.log
+        free -t >> /home/$user/scripts/script3.log
+        df >> /home/$user/scripts/script3.log
 fi;
